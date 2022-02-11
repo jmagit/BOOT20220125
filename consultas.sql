b0b03391cb55a1bb1ce56c0a0fb76253ab513612
Javier <jmartin@grupoloyal.com>
2022-02-11 13:34:16 +0100

    consultas

--- consultas.sql
+++ consultas.sql
@@ -25,3 +25,51 @@ SELECT if(GROUPING(rating) = 1, 'TOTAL', rating) Clasificacion,
 FROM film
 GROUP BY rating, rental_rate WITH rollup
 ORDER BY GROUPING(rating), rating, GROUPING(rental_rate), rental_rate
+
+SELECT rating Clasificacion, 
+	case rental_rate 
+	when 0.99 then 'baja' 
+	when 2.99 then 'media' 
+	when 4.99 then 'alta'
+	ELSE 'desconocida'
+	END gama, count(title) `Nº Peliculas`, AVG(LENGTH) `Duración media`
+FROM film
+WHERE rating NOT IN ('g', 'pg')
+GROUP BY rating, rental_rate
+HAVING COUNT(*) > 0
+
+SELECT title, rental_rate, 'Peli' origen
+FROM film
+WHERE rating NOT IN ('g', 'pg')
+UNION
+SELECT CONCAT(first_name, ' ', last_name) actor, 0, 'Actor'
+FROM actor
+
+
+t1: 2cx10
+t2: 10cx100
+t2: 3cx5
+
+SELECT *
+FOR staff empleados INNER JOIN staff jefes ON empleados.jefe_id = jefes.staff_id
+
+SELECT title, f.rating, rental_rate, media
+	FROM film f INNER JOIN 
+		(SELECT rating, avg(rental_rate) media from film GROUP BY rating) m
+	ON f.rating = m.rating
+WHERE rental_rate > media
+
+SELECT title, f.rating, rental_rate, 
+	(SELECT avg(rental_rate) from film where rating = f.rating) media
+	FROM film f 
+WHERE rental_rate > (SELECT avg(rental_rate) from film where rating = f.rating)
+
+
+
+
+SELECT ROW_NUMBER () over(ORDER  BY LENGTH) NumLinea, film_id,
+title, LENGTH, NTILE(5) over(ORDER BY length)
+FROM film
+ORDER BY NumLinea
+
+
