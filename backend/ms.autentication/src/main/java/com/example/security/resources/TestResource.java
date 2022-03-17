607a1c8829a65b51fd1c8977465c293393836c05
Javier <jmartin@grupoloyal.com>
2022-03-17 12:38:15 +0100

    Token de seguridad

--- backend/ms.autentication/src/main/java/com/example/security/resources/TestResource.java
+++ backend/ms.autentication/src/main/java/com/example/security/resources/TestResource.java
@@ -1,17 +1,20 @@
 package com.example.security.resources;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestResource {
 
 	@GetMapping("/seguro")
-	public String get() {
-		return "El usuario está autenticado";
+	public String get(@RequestHeader String authorization, Principal principal) {
+		return "El usuario está autenticado: " + principal.getName() + "\nAuthorization: " + authorization;
 	}
 	
 	@GetMapping("/admin")
