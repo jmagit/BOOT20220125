a117bd977cd0f074538a0c21be5092f36be5af50
Javier <jmartin@grupoloyal.com>
2022-03-18 10:14:51 +0100

    test

---
+++ frontend/spec/ejercicios.spec.js
@@ -0,0 +1,38 @@
+describe('Pruebas de los ejercicios', () => {
+    describe('Ejercicio 1', () => {
+        describe('OK', () => {
+            it('Genera aleatorio', () => {
+                let num = aleatorio(1, 100);
+                expect(num).toBeGreaterThanOrEqual(1)
+                expect(num).toBeLessThanOrEqual(100)
+            })
+        });
+    
+        describe('KO', () => {
+            it('Falta parámetro', () => {
+                expect(() => aleatorio(1, 100)).toThrow(); 
+                pending('esto es por lo que está pendiente');
+            })
+        });
+    
+            
+    });
+
+    describe('Ejercicio 2', () => {
+        it('Pendiente')
+    });
+
+    describe('Ejercicio 5', () => {
+        describe('NIF OK', () => {
+            ['12345678z', '12345678Z', '1234S', '4g'].forEach(caso => {
+                it(`NIF: ${caso}`, () => expect(esNIF(caso)).toBeTrue())
+            })
+        });
+    
+        describe('NIF KO', () => {
+            ['1234J', '12345678', 'Z', 'Z12345678'].forEach(caso => {
+                it(`NIF: ${caso}`, () => expect(esNIF(caso)).toBeFalse())
+            })
+        });
+    });
+})
