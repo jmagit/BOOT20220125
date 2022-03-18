81c04a734c24bd49a6d535808abe753c52c285b0
Javier <jmartin@grupoloyal.com>
2022-03-18 12:41:30 +0100

    e2e

--- frontend/spec/ejercicios.spec.js
+++ frontend/spec/ejercicios.spec.js
@@ -2,20 +2,22 @@ describe('Pruebas de los ejercicios', () => {
     describe('Ejercicio 1', () => {
         describe('OK', () => {
             it('Genera aleatorio', () => {
+                let spy = spyOn(Math, "random").and.returnValue(0.496003)
                 let num = aleatorio(1, 100);
+                expect(spy).toHaveBeenCalled()
                 expect(num).toBeGreaterThanOrEqual(1)
                 expect(num).toBeLessThanOrEqual(100)
+                expect(num).toBe(50)
             })
         });
     
         describe('KO', () => {
             it('Falta parámetro', () => {
-                expect(() => aleatorio(1, 100)).toThrow(); 
+                expect(() => aleatorio(1)).toThrow(); 
                 pending('esto es por lo que está pendiente');
             })
         });
-    
-            
+           
     });
 
     describe('Ejercicio 2', () => {
