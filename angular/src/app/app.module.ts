1e4c64f9b7d0e0e1c71bc5b7c96b3c38cf7da5fd
Javier <jmartin@grupoloyal.com>
2022-03-22 14:04:38 +0100

    modulos

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MyCoreModule } from 'src/lib/my-core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainModule } from './main';
+import { SecurityModule } from './security';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule, MainModule, SecurityModule, MyCoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/src/app/main/main.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModule {
+  constructor( @Optional() @SkipSelf() parentModule: MainModule) {
+    if (parentModule) {
+      const msg = `MainModule has already been loaded.
+        Import MainModule once, only, in the root AppModule.`;
+      throw new Error(msg);
+    }
+  }
+}
