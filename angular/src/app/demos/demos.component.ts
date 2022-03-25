803f8ff0019180f1a740c0706bbe7e943aa25e45
Javier <jmartin@grupoloyal.com>
2022-03-25 13:08:25 +0100

    RxJS

--- angular/src/app/demos/demos.component.ts
+++ angular/src/app/demos/demos.component.ts
@@ -79,11 +79,11 @@ export class DemosComponent implements OnInit, OnDestroy {
 
   private suscriptor: Unsubscribable | undefined;
   ngOnInit(): void {
-    // this.suscriptor = this.vm.Notificacion.subscribe(n => {
-    //   if (n.Type !== NotificationType.error) { return; }
-    //   window.alert(`Suscripcion: ${n.Message}`);
-    //   this.vm.remove(this.vm.Listado.length - 1);
-    // });
+    this.suscriptor = this.vm.Notificacion.subscribe(n => {
+      if (n.Type !== NotificationType.error) { return; }
+      window.alert(`Suscripcion: ${n.Message}`);
+      this.vm.remove(this.vm.Listado.length - 1);
+    });
   }
   ngOnDestroy(): void {
     if (this.suscriptor) {
